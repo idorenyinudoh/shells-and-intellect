b97eadcd49bc157c8b262b465c8af649bbf1f878
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2025-02-19 01:33:34 +0100

    feat: create default layout

--- app.vue
+++ app.vue
@@ -1,5 +1,48 @@
 <template>
-  <div>
-    <p>shells and intellect</p>
+  <div class="page">
+    <header>
+      <div class="content">
+        <NuxtPage />
+      </div>
+    </header>
   </div>
 </template>
+
+<style lang="scss">
+:root {
+  --header-height: 24vh;
+  --content-margin-inline: 7.5%;
+}
+
+@media screen and (min-width: 768px) {
+  :root {
+    --header-height: 29.4vh;
+    --content-margin-inline: 11.25%;
+  }
+}
+
+@media screen and (min-width: 1652px) {
+  :root {
+    --content-margin-inline: auto;
+  }
+}
+
+.page {
+  min-height: 100dvh;
+  background-color: #FFF;
+
+  header {
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    height: var(--header-height);
+    background-color: #DCE3EA;
+  }
+
+  .content {
+    margin-top: calc(0.4 * var(--header-height));
+    margin-inline: var(--content-margin-inline);
+    max-width: 1280px;
+  }
+}
+</style>
